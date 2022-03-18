0bfc7eeb73b80d2bee76441b1d11a24026ce9256
MikeC42001 <miguelsscosta3@gmail.com>
2022-03-18 12:17:54 +0000

    4 files modified - ex2 part 2

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,18 +1,17 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', pet: { id: 21, name: 'Landreti' } },
+  { id: 12, name: 'Narco', pet: { id: 12, name: 'Octavio' } },
+  { id: 13, name: 'Bombasto', pet: { id: 13, name: 'BestaBesta' } },
+  { id: 14, name: 'Celeritas', pet: { id: 14, name: 'Chirichachi' } },
+  { id: 15, name: 'Magneta', pet: { id: 15, name: 'Magma' } },
+  { id: 16, name: 'RubberMan', pet: { id: 16, name: 'SuperSuper' } },
+  { id: 17, name: 'Dynama', pet: { id: 17, name: 'Escorrega' } },
+  { id: 18, name: 'Dr IQ', pet: { id: 18, name: '-IQ' } },
+  { id: 19, name: 'Magma', pet: { id: 19, name: 'LumberJack' } },
+  { id: 20, name: 'Tornado', pet: { id: 20, name: 'FuriFuri' } },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
